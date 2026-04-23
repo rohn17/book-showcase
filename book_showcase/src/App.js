d143ebf4584b90c45aa22c2b3d358163043b03de
rohn17 <rohanchoudhary.us17@gamil.com>
2026-04-23 17:10:41 +0530

    done

--- book_showcase/src/App.js
+++ book_showcase/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BookDetails from "./pages/BookDetails";
+import Favorites from "./pages/Favorites";
+import Navbar from "./components/Navbar";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FavoritesProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </Router>
+    </FavoritesProvider>
   );
 }
 
---
+++ book_showcase/src/components/Loader.js
@@ -0,0 +1,5 @@
+function Loader() {
+  return <h3 className="loader">Loading...</h3>;
+}
+
+export default Loader;
